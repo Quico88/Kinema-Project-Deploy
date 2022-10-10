0e55000c37ff2eb1ea760528c90523613c843f77
Pablo Carro <pablo.carro@live.com>
2022-10-10 19:39:15 -0300

    search controller
    finzalice la ruta de search

--- api/src/controlers API/searchbar-controller.js
+++ api/src/controlers API/searchbar-controller.js
@@ -1,5 +1,7 @@
-import { axios } from axios;
-
+const axios = require('axios');
+const e = require('express');
+require('dotenv').config();
+const { YOUR_API_KEY_1 } = process.env
 //Funcion para hacer el request a la api de MULTISEARCH, si se le pasa un param (sea nombre de peli, serie o actor)
 //lo manda por query y busca sobre la API lo relacionado, si no se le pasa nada, te trae el discover directamente.
 
@@ -7,23 +9,26 @@ const getSearchMulti = async (search) => {
   const type = search ? 'search' : 'discover';
   const change = search ? 'multi' : 'movie';
 
-  const apiURL = await axios.get(`https://api.themoviedb.org/3/${type}/${change}`, {
+  const {data: {results}} = await axios.get(`https://api.themoviedb.org/3/${type}/${change}`, {
     params: {
-      api_key: '83d353826e0412f846edff6b5f421ad9',
+      api_key: YOUR_API_KEY_1,
       query: search
     }
   });
 
-  const apiINFO = await apiURL.results.map(m => {
+  const movieSearch = results.map(m => {
     return {
       id: m.id,
-      title: title,
-      overview: m.overview,
-      popularity: m.popularity,
-      image: m.poster_path,
-      genres : m.genre_ids.map(g => g)
+      title: m.title,
+      type: m.media_type,
+      description: m.overview,
+      poster: m.poster_path,
+      backPoster: m.backdrop_path,
     }
   })
+  console.log(movieSearch)
+}
 
-  return apiINFO;
+module.exports = {
+  getSearchMulti
 }
