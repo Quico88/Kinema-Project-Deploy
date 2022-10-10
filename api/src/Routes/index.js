aeced518b0a6e0a7666454269c75c7a50a24e18e
Federico Irarrazaval <fedeirar@gmail.com>
2022-10-10 12:44:45 -0300

    Base
    Base

---
+++ api/src/Routes/index.js
@@ -0,0 +1,9 @@
+const { Router } = require('express');
+const router = Router();
+// const { Movies, Genres } = require('../db.js');
+
+// Import functions from controllers:
+
+// ROUTES:
+
+module.exports = router;
