0b3b4d5c1d3e452a269d0b82579a0d3c85e50bd0
MatiTonelli <matias.tonelli@outlook.com>
2022-10-11 18:44:30 -0300

    Create Carrousel.jsx

---
+++ client/src/Components/Carrousel/Carrousel.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Carousel from "react-elastic-carousel";
+import { Link } from "react-router-dom";
+
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 2 },
+  { width: 768, itemsToShow: 3 },
+  { width: 1200, itemsToShow: 4 },
+  { width: 1600, itemsToShow: 5 },
+];
+
+export default function Carrousel({ movies, routeType }) {
+  return (
+    <div>
+      <Carousel breakPoints={breakPoints}>
+        {movies.map((m) => {
+            if(m.media_type === 'movie'){
+                return (
+                    <Link to={'home/movie_details'}>
+                      <img
+                        src={"https://image.tmdb.org/t/p/w300" + m.poster_path}
+                      ></img>
+                    </Link>
+                  );
+            }
+            if(m.media_type === 'tv'){
+                return (
+                    <Link to={'home/tv_show_details'}>
+                      <img
+                        src={"https://image.tmdb.org/t/p/w300" + m.poster_path}
+                      ></img>
+                    </Link>
+                  );
+            }
+          return (
+            <Link to={'home'}>
+              <img
+                src={"https://image.tmdb.org/t/p/w300" + m.poster_path}
+              ></img>
+            </Link>
+          );
+        })}
+      </Carousel>
+    </div>
+  );
+}
