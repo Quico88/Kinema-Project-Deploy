2737b2df36ff1a8874470e1dfe4c70d666299de2
Adriel Lopez <80710274+AdrielLopez@users.noreply.github.com>
2022-10-11 22:43:07 -0300

    MovieDetail
    Componente MovieDetail con sus actions y reducer modificados

--- client/src/Redux/reducer/index.js
+++ client/src/Redux/reducer/index.js
@@ -1,7 +1,25 @@
+import { CLEAR_MOVIE_DETAIL, GET_MOVIE_DETAIL  } from "../actions/const";
 const initialState = {
   movies: [],
+  movieDetail: [],
 };
 
-const rootReducer = (state = initialState, action) => {};
+const rootReducer = (state = initialState, action) => {
+  switch (action.type)
+  {
+    case GET_MOVIE_DETAIL:
+      return {
+        ...state,
+        movieDetail: action.payload
+      }
+    case CLEAR_MOVIE_DETAIL:
+      return {
+        ...state,
+        movieDetail: []
+      }
+    default: return state;
+  }
+  
+};
 
 export default rootReducer;
