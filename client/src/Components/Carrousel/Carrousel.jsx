178a534036de5efe1d572f22494a3a9ff0d9a4da
MatiTonelli <matias.tonelli@outlook.com>
2022-10-12 17:08:13 -0300

    Carrousel funcional

--- client/src/Components/Carrousel/Carrousel.jsx
+++ client/src/Components/Carrousel/Carrousel.jsx
@@ -1,39 +1,19 @@
 import React from "react";
-import Carousel from "react-elastic-carousel";
+import Carousel from "./Chackra UI Components/CarouselChackra";
 import { Link } from "react-router-dom";
+import styles from "./Carrousel.module.css";
 
 const breakPoints = [
-  { width: 1, itemsToShow: 1 },
-  { width: 550, itemsToShow: 2 },
-  { width: 768, itemsToShow: 3 },
-  { width: 1200, itemsToShow: 4 },
-  { width: 1600, itemsToShow: 5 },
-];
+        { width: 1, itemsToShow: 1 },
+        { width: 550, itemsToShow: 2 },
+        { width: 768, itemsToShow: 3 },
+        { width: 1200, itemsToShow: 4 },
+        { width: 1600, itemsToShow: 5 },
+      ];
 
-export default function Carrousel({ movies, routeType }) {
+export default function Carrousel({movies}) {
+    
   return (
-    <div>
-      <Carousel breakPoints={breakPoints}>
-        {movies.map((m) => {
-            if(routeType === 'movie'){
-                return (
-                    <Link to={'home/movie_details'}>
-                      <img
-                        src={"https://image.tmdb.org/t/p/w300" + m.poster_path}
-                      ></img>
-                    </Link>
-                  );
-            } else {
-                return (
-                    <Link to={'home/tv_show_details'}>
-                      <img
-                        src={"https://image.tmdb.org/t/p/w300" + m.poster_path}
-                      ></img>
-                    </Link>
-                  );
-            }
-        })}
-      </Carousel>
-    </div>
+    <Carousel movies={movies}></Carousel>
   );
 }
