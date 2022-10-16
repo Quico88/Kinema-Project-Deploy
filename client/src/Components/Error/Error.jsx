fc34ffc153aa575a2e6a76e429ab29bec77fbd27
Federico Irarrazaval <fedeirar@gmail.com>
2022-10-16 13:02:38 -0300

    improve error
    Le puse detalle a la devolución del error.

--- client/src/Components/Error/Error.jsx
+++ client/src/Components/Error/Error.jsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 import cleanError from '../../Redux/actions';
 import Footer from '../Home/Chakra UI Components/Footer';
 import NavBar from '../NavBar/NavBar.jsx';
+import tv_screen_error from '../../Assets/tv_screen_error.png';
 
 const Error = () => {
   const dispatch = useDispatch();
@@ -19,14 +20,43 @@ const Error = () => {
   };
 
   return (
-    <Box>
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="space-between"
+      height="100vh"
+    >
       <NavBar />
-      <Alert status="error">
-        <AlertIcon />
-        <AlertTitle mr={2}>
-          Apologies. We have encountered an error. Please try again.
-        </AlertTitle>
-        <Button onClick={handleError}>Go back.</Button>
+      <Alert
+        status="error"
+        variant="subtle"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        textAlign="center"
+        height="100%"
+      >
+        <Box
+          backgroundImage={`url(${tv_screen_error})`}
+          backgroundSize="cover"
+          backgroundPosition="center"
+          padding="40px"
+        >
+          <AlertIcon boxSize="40px" mr={0} margin="auto" />
+          <AlertTitle mt={4} mb={1} fontSize="lg">
+            Oops! Something went wrong.
+          </AlertTitle>
+          <Button
+            onClick={handleError}
+            colorScheme="red"
+            variant="outline"
+            size="lg"
+            mt={4}
+            mb={4}
+          >
+            Go Back
+          </Button>
+        </Box>
       </Alert>
       <Footer />
     </Box>
