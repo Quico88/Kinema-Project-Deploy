7e70e1fe2c3a6832ed1886c56bb8e80f839bc0ff
Juan Francisco Drewanz <juandrewanz@hotmail.com>
2022-10-16 19:15:13 -0300

    testing route

--- client/src/index.js
+++ client/src/index.js
@@ -16,9 +16,9 @@ import './index.css';
 // Deploy:
 import axios from 'axios';
 
-import dotenv from 'dotenv';
-dotenv.config();
-axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001';
+// import dotenv from 'dotenv';
+// dotenv.config();
+axios.defaults.baseURL = 'https://kinema-project-deploy-production.up.railway.app/' || 'http://localhost:3001';
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
