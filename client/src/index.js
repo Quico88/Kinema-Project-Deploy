8409970c115d11242af2c582e236a83ccea6b7e8
Federico Irarrazaval <fedeirar@gmail.com>
2022-10-17 10:30:22 -0300

    nav bar and overflow hidden

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 
 // Import app component and store:
 import App from './App';
@@ -14,15 +14,17 @@ import { store } from './Redux/store/index.js';
 import './index.css';
 
 //Import fonts:
-import "@fontsource/nunito"
-import "@fontsource/raleway"
+import '@fontsource/nunito';
+import '@fontsource/raleway';
 
 // Deploy:
 import axios from 'axios';
 
 // import dotenv from 'dotenv';
 // dotenv.config();
-axios.defaults.baseURL = 'https://kinema-project-deploy-production.up.railway.app/' || 'http://localhost:3001';
+axios.defaults.baseURL =
+  'https://kinema-project-deploy-production.up.railway.app/' ||
+  'http://localhost:3001';
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
@@ -31,14 +33,14 @@ const theme = extendTheme({
   styles: {
     global: () => ({
       body: {
-        bg: "",
+        bg: '',
       },
     }),
   },
   fonts: {
-    heading: "Raleway",
-    body: "Nunito"
-  }
+    heading: 'Raleway',
+    body: 'Nunito',
+  },
 });
 
 root.render(
@@ -49,4 +51,4 @@ root.render(
       </BrowserRouter>
     </Provider>
   </ChakraProvider>
-)
+);
