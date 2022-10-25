2df78bb54b85f42971be6fa454fb9c9a8f0a706b
GusEngers <gustavoengers6441@gmail.com>
2022-10-25 14:54:22 -0300

    habilitando carrusels

--- api/src/controllers local/getDataCarruselsJSON.js
+++ api/src/controllers local/getDataCarruselsJSON.js
@@ -3,6 +3,7 @@ const populars = require('../backup/carrusels_movies/populars_movies.json');
 const topRated = require('../backup/carrusels_movies/topRated_movies.json');
 const trending = require('../backup/carrusels_movies/trending_movies.json');
 const upComing = require('../backup/carrusels_movies/upComing_movies.json');
+const series_backup = require('../backup/BackUpSeries.json');
 
 const on_theatersData = () => {
 	return on_theaters;
@@ -24,10 +25,24 @@ const upComingData = () => {
 	return upComing;
 };
 
+const topRatedSeries = () => {
+	return series_backup
+		.sort((a, b) => b.rating - a.rating)
+		.slice(0, 20);
+};
+
+const latestSeries = () => {
+	return series_backup
+		.sort((a, b) => Date.parse(b.release_date_first_episode) - Date.parse(a.release_date_first_episode))
+		.slice(0, 20);
+}
+
 module.exports = {
 	on_theatersData,
 	popularsData,
 	topRatedData,
 	trendingData,
-	upComingData
+	upComingData,
+	topRatedSeries,
+	latestSeries
 }
