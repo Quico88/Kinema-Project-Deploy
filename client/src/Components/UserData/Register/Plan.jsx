4f87c9326f48d03d33e7c66c6c94a9302c96fbcb
Juan Francisco Drewanz <juandrewanz@hotmail.com>
2022-10-30 11:37:41 -0300

    Responsive plan detail

--- client/src/Components/UserData/Register/Plan.jsx
+++ client/src/Components/UserData/Register/Plan.jsx
@@ -1,16 +1,9 @@
-import React, { useEffect, useState } from "react";
-import {
-  Box,
-  Stack,
-  Text,
-  Container,
-  SimpleGrid
-} from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import { Box, Stack, Text, Container, SimpleGrid } from '@chakra-ui/react';
 import Pricing from './Pricing';
 import Footer from '../../Home/Chakra UI Components/Footer';
 
 export default function Plan() {
-
   const [scroll, setScroll] = useState(false);
 
   useEffect(() => {
@@ -26,53 +19,52 @@ export default function Plan() {
 
   return (
     <div>
-    <Box
-      position={'relative'}
-      height={'100vh'}
-      backgroundColor={'#1D1D1D'}
-      backgroundImage={
-        'linear-gradient(180deg, #0000008c 20%, #0000008c 100%), url(https://www.lavanguardia.com/files/og_thumbnail/uploads/2020/12/14/5fd73c24bebce.jpeg)'
-      }
-      backgroundRepeat={'no-repeat'}
-      backgroundSize={'cover'}
+      <Box
+        position={'relative'}
+        minHeight={'100vh'}
+        backgroundColor={'#1D1D1D'}
+        backgroundImage={
+          'linear-gradient(180deg, #0000008c 20%, #0000008c 100%), url(https://www.lavanguardia.com/files/og_thumbnail/uploads/2020/12/14/5fd73c24bebce.jpeg)'
+        }
+        backgroundRepeat={'no-repeat'}
+        backgroundSize={'cover'}
       >
-      <Container
-        as={SimpleGrid}
-        maxW={'7xl'}
-        columns={{ base: 1, md: 2 }}
-        spacing={{ base: 10, lg: 32 }}
+        <Container
+          as={SimpleGrid}
+          maxW={'7xl'}
+          columns={{ base: 1, md: 2 }}
+          spacing={{ base: 10, lg: 32 }}
           py={{ base: 10, sm: 20, lg: 32 }}
-          display="flex"
-          alignItems={"center"}
-        justifyContent="center"  
+          display='flex'
+          flexDirection= {{base: 'column', sm: 'row'}}
+          alignItems={'center'}
+          justifyContent='center'
         >
           <Text
-            fontSize={"4vh"}
-            fontWeight="600"
-            display="inline"
-            color={"white"}
-            >Select your plan:
-           </Text>
-         <Stack direction={{ base: 'flex', sm: 'row' }}>
-            <Pricing 
-            planType={"Basic"}
-            price={0}
-            firstFeature={"Rent Movies and TV Shows"}
-           />
-          <Pricing 
-            planType={"Premium"} 
-            price={7.99}
-            firstFeature={"Watch any Movie and TV Show"}
-            secondFeature={"Create your own Watchlist"}
-            thirdFeature={"All features"} 
-           />
-         
-        </Stack>
-        
-      </Container>
-    </Box>
-      <Footer/>
+            fontSize={'4vh'}
+            fontWeight='600'
+            display='inline'
+            color={'white'}
+          >
+            Select your plan:
+          </Text>
+          <Stack direction={{ base: 'column', sm: 'row' }}>
+            <Pricing
+              planType={'Basic'}
+              price={0}
+              firstFeature={'Rent Movies and TV Shows'}
+            />
+            <Pricing
+              planType={'Premium'}
+              price={7.99}
+              firstFeature={'Watch any Movie and TV Show'}
+              secondFeature={'Create your own Watchlist'}
+              thirdFeature={'All features'}
+            />
+          </Stack>
+        </Container>
+      </Box>
+      <Footer />
     </div>
   );
 }
-
