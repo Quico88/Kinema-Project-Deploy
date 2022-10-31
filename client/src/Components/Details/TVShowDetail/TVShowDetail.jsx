2ee582e0f165257ec706393bb118eb0e50713b1c
MatiTonelli <matias.tonelli@outlook.com>
2022-10-31 14:29:20 -0300

    fix

--- client/src/Components/Details/TVShowDetail/TVShowDetail.jsx
+++ client/src/Components/Details/TVShowDetail/TVShowDetail.jsx
@@ -77,6 +77,7 @@ export default function TVShowDetail() {
     dispatch(isLike(user.uid, id));
     dispatch(getLikesFromContent(id));
     dispatch(loadUserData(user.uid));
+    dispatch(getCommentsData(id));
   }, [dispatch]);
 
   if (user && user.banned) {
